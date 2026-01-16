4a94649d52d346b16ea7b8db7af52ac3687577ad
Arshadh-tradebrains <arshadh.p@tradebrains.in>
2026-01-16 14:56:28 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/mintly/stackforge/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mintly.stackforge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/utils/extensions/media_query_extensions.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+extension MediaQueryExtensions on BuildContext {
+  double get screenHeight => MediaQuery.of(this).size.height;
+  double get screenWidth => MediaQuery.of(this).size.width;
+  Size get screenSize => Size(screenWidth, screenHeight);
+}
